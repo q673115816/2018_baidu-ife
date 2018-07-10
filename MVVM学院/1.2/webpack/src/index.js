26da22bc30d8e44ad2df66141bab5dd20177e9b7
s871_linjinjun <673115816@qq.com>
2018-07-10 18:19:01 +0800

    1.2

--- MVVM学院/1.2/webpack/src/index.js
+++ MVVM学院/1.2/webpack/src/index.js
@@ -1,3 +1,6 @@
+import _ from 'lodash'
+import './style.css'
+import Icon from './icon.png'
 
 const a = 111;
 console.log(a);
@@ -5,6 +8,11 @@ console.log(a);
 function component(){
     let element = document.createElement('div')
     element.innerHTML = _.join(['Hello','Webpack'],' ')
+    element.classList.add('hello')
+
+    var MyIcon = new Image()
+    MyIcon.src = Icon
+    element.appendChild(MyIcon)
     return element
 }
 
--- MVVM学院/1.2/webpack/webpack.config.js
+++ MVVM学院/1.2/webpack/webpack.config.js
@@ -1,17 +1,34 @@
-
 const path = require('path')
 
-
 const config = {
-    mode: 'production',
     entry: './src/index.js',
     output: {
-        path: {
-            path: path.join(__dirname,'dist'),
-            filename: 'bundle.js'
-        }
+        filename: 'bundle.js',
+        path: path.resolve(__dirname,'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /.\(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
     }
-    
 }
 
 module.exports = config
