c3ca89009198198926ed9e4090d1e53be2b18516
s871_linjinjun <673115816@qq.com>
2018-07-11 16:53:29 +0800

    1.2

--- MVVM学院/1.2/webpack/src/index.js
+++ MVVM学院/1.2/webpack/src/index.js
@@ -2,14 +2,11 @@ import san from "san";
 import { router } from "san-router";
 import San from "./app.san";
 
+let body = '123'
+console.log(body)
+
 console.log('hello webpack  San')
 
 router.add({rule: '/',Component:San, target: '#app'})
 
 router.start()
-
-var MyApp = san.defineComponent()
-
-var myApp = new MyApp()
-
-myApp.attach(document.body)
