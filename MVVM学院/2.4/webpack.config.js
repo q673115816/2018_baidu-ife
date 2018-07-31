fb25b7f69034023216021fed3c04bce623d9377f
s871_linjinjun <673115816@qq.com>
2018-07-31 15:24:28 +0800

    2.4

--- MVVM学院/2.4/webpack.config.js
+++ MVVM学院/2.4/webpack.config.js
@@ -1,24 +1,29 @@
 const path = require('path')
-const htmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
 const config = {
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: 'bundle.js'
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
                 test: /.san$/,
                 use: ['san-loader']
             }
+
         ]
     },
     plugins: [
-        new htmlWebpackPlugin({
-            template: './index.html'
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title:'Hello World',
+            template:'./index.html'
         })
     ]
 }
 
-module.exports = config 
+module.exports = config
