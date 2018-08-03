33e4f0d360edfdc0935de569f9048e761cc34120
s871_linjinjun <673115816@qq.com>
2018-08-03 17:52:57 +0800

    2.7.2

---
+++ MVVM学院/2.7.2/src/app.js
@@ -0,0 +1,4 @@
+import san from "san";
+import app from "./app.san";
+
+new app().attach(document.querySelector('#app'))
---
+++ MVVM学院/2.7.2/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+const config = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /.san$/,
+                use: ['san-loader']
+            },
+            {
+                test: /.css$/,
+                use: ['style-loader','css-loader']
+            }
+
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title:'Hello World',
+            template:'./index.html'
+        })
+    ]
+}
+
+module.exports = config
